16b24ec14f898a1111b0b2216e8016ce737ec56c
Kostya <zarechnev.kons@gmail.com>
2023-08-27 18:30:51 +0600

    feat: ranking entity

---
+++ api/src/ranking/entities/ranking.entities.ts
@@ -0,0 +1,20 @@
+import {Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {Fighter} from "../../fighter/entities/fighter.entity";
+import {WeightClass} from "../../weight__class/entities/weight__class.entity";
+
+@Entity('rankings')
+export class Ranking {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => Fighter, (fighter) => fighter.id, {lazy: true})
+    @JoinColumn({name: 'fighter'})
+    fighter: Fighter;
+
+    @ManyToOne(() => WeightClass, (weight) => weight.id, {lazy: true})
+    @JoinColumn({name: 'weight_class'})
+    weight_class: WeightClass;
+
+    @Column({nullable: false})
+    rank: number;
+}
